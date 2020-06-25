b55da6057cc667fd6996a477de6ed603495fd9ee
Mitesh Patel <techno.mitesh@excellenceinfonet.com>
2020-06-25 19:10:09 +0530

    Image Upload in Azure Storage

---
+++ Architechture.Web/Models/AzureStorageConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace Architecture.Web.Models
+{
+    public class AzureStorageConfig
+    {
+        public string AccountName { get; set; }
+        public string AccountKey { get; set; }
+        public string ImageContainer { get; set; }
+        public string ThumbnailContainer { get; set; }
+        public string ConnectionString { get; set; }
+    }
+}
